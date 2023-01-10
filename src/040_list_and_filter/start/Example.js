69656bcae691a7658f617faa8357d9f6808e6978
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-10 20:53:43 +0900

    filterメソッド追加

--- src/040_list_and_filter/start/Example.js
+++ src/040_list_and_filter/start/Example.js
@@ -3,13 +3,21 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterVal, serFilterVal] = useState("");
+  console.log(animals.filter((animal) => animal === "Dog"));
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input type="text" value={filterVal} onChange={(e) => serFilterVal(e.target.value)} />
       <ul>
         {animals
+          .filter(animal => {
+            const isMatch = animal.indexOf(filterVal) !==-1;
+            console.log(animal.indexOf(filterVal))
+            return isMatch
+          })
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
