0a1e4d828c8723ca87853c7387888023dedd4045
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-11 00:50:21 +0900

    🔧複数のチェックボックスに対応するコード追記

--- src/110_multi_checkbox/start/Example.js
+++ src/110_multi_checkbox/start/Example.js
@@ -9,9 +9,53 @@ const Example = () => {
 
   const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {
+    const newFruits = fruits.map((fruit) => {
+      const newFruit = { ...fruit };
+      if (newFruit.label === e.target.value) {
+        newFruit.checked = !fruit.checked;
+      }
+
+      return newFruit;
+    });
+
+    setFruits(newFruits);
+
+    let sumVal = 0;
+    // newFruits.forEach(fruit => {
+    //   if (fruit.checked) {
+    //     sumVal += fruit.value
+    //   }
+    // });
+
+    newFruits
+      .filter((fruit) => fruit.checked)
+      .forEach((fruit) => {
+        sumVal += fruit.value;
+      });
+
+    setSum(sumVal);
+  };
+
   return (
     <div>
-      
+      {fruits.map((fruit) => {
+        return (
+          <div key={fruit.label}>
+            <input
+              type="checkbox"
+              value={fruit.label}
+              onChange={handleChange}
+              name=""
+              id={fruit.label}
+              checked={fruit.checked}
+            />
+            <label htmlFor={fruit.label}>
+              {fruit.label}:{fruit.value}
+            </label>
+          </div>
+        );
+      })}
       <div>合計：{sum}</div>
     </div>
   );
