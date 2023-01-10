430e571580d9afa7c9987ae7b09b308674cba245
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-10 23:29:54 +0900

    🔧ラジオボタンをmapメソッドで書き換え

--- src/090_radio/start/Example.js
+++ src/090_radio/start/Example.js
@@ -1,10 +1,55 @@
+import { useState } from "react";
+
+// POINT ラジオボタンの実装
 const Example = () => {
+  const [fruit, setFruit] = useState("Apple");
+  const onChange = (e) => setFruit(e.target.value);
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        );
+      })}
+      {/* <label>
+        <input
+          type="radio"
+          value="Apple"
+          checked={fruit === "Apple"}
+          onChange={onChange}
+        />
+        Apple
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Banana"
+          checked={fruit === "Banana"}
+          onChange={onChange}
+        />
+        Banana
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Cherry"
+          checked={fruit === "Cherry"}
+          onChange={onChange}
+        />
+        Cherry
+      </label> */}
+      <h3>私は{fruit}がたべたい</h3>
+    </>
   );
 };
 
